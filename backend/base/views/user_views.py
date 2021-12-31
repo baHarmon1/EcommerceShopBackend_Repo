283461cfbc21d1e909515b3acf1f35887bbc4a04
Brycen Harmon <brycenharmon@gmail.com>
2021-12-31 09:56:30 -0600

    Created a folder for views and urls to clean up the views and urls file, also deleted the files urls.py and views.py

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,14 +1,11 @@
 from django.contrib.auth.models import User, update_last_login
 from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework import serializers
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
-from .models import Product
-from .products import products
-from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -49,21 +46,6 @@ def registerUser(request):
         message = {'detail':'User with this email already exists'}
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
-
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        'api/products/',
-        'api/products/create/',
-        'api/products/upload/',
-        'api/products/<id>/reviews/',
-        'api/products/top/',
-        'api/products/<id>/',
-        'api/products/delete/<id>/',
-        'api/products/<update>/<id>/',
-    ]
-    return Response(routes)
-
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getUserProfile(request):
@@ -76,16 +58,4 @@ def getUserProfile(request):
 def getUsers(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def getProduct(request, pk):
-    product = Product.objects.get(_id=pk)
-    serializer = ProductSerializer(product)
     return Response(serializer.data)
